20072a6660d92137b407478ba2a4cc9f0c073784
francis <francismutua664@gmail.com>
2022-10-29 13:22:46 +0300

    new commit

--- app/src/main/java/com/example/digiseedbed/AdminDashBoard.java
+++ app/src/main/java/com/example/digiseedbed/AdminDashBoard.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -22,6 +25,13 @@ public class AdminDashBoard extends AppCompatActivity {
         workers=findViewById(R.id.workers);
         add_customer=findViewById(R.id.add_customer);
         add_worker=findViewById(R.id.add_worker);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+        }
         progress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
